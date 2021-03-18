d7c7de3d201b79876ba18f453bdab139c45ebf3e
TangoBr <thomasbridges01@gmail.com>
2021-03-17 22:31:15 -0400

    get

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 const express = require(`express`);
 const app = express();
+const path = require(`path`);
+const router = express.router();
 
-app.get()
+app.get(`/dashboard`, (req, res) => {
+    res.sendFile(path.join(__dirname, `..`, `html`, `dashboard.html`))
+});
+
+app.get(`/login`, (req, res) => {
+    res.sendFile(path.join(__dirname, `..`, `html`, `login.html`))
+});
+
+app.get(`/register`, (req, res) => {
+    res.sendFile(path.join(__dirname, `..`, `html`, `register.html`))
+});
+
+app.listen(3000, () => {
+    console.log(`>>>SERVER STARTED<<<`)
+});
+
